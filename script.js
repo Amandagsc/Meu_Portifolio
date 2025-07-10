bb63735836b9d279fcbb63a2ac5f13912a53b3e9
Aanda Carvalho <amanda99carvalho2018@gmail.com>
2025-07-10 08:09:50 -0300

    inclementando página de agradecimento.html

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-document.getElementById('form-contato').addEventListener('submit', function (event) {
-  event.preventDefault(); // Bloqueia o envio normal
+  function enviarWhatsApp() {
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const mensagem = document.getElementById('mensagem').value;
 
-  const nome = document.getElementById('nome').value;
-  const email = document.getElementById('email').value;
-  const mensagem = document.getElementById('mensagem').value;
+    const texto = `Olá, meu nome é ${nome}%0AEmail: ${email}%0AMensagem: ${mensagem}`;
 
-  const texto = `Olá, meu nome é ${nome}%0AEmail: ${email}%0AMensagem: ${mensagem}`;
-  const numero = '5531988040607'; // Seu WhatsApp com DDI
+    // Substitua o número abaixo pelo seu número do WhatsApp, no formato internacional
+    const numero = '5531988040607'; // Exemplo com DDD 31
 
-  // Abre o WhatsApp com a mensagem
-  window.open(`https://wa.me/${numero}?text=${texto}`, '_blank');
-});
+    window.open(`https://wa.me/${numero}?text=${texto}`, '_blank');
+    setTimeout(() => {
+      window.location.href = "agradecimento.html";
+    }, 2000);
+  }
